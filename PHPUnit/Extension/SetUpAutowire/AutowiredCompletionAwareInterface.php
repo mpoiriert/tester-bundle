ca5e401e1d00b2e9311e118de321b814f316a796
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-08-11 18:01:48 -0400

    [Tester\TesterBundle] Refactor PHPUnit extension and improve documentation

--- PHPUnit/Extension/SetUpAutowire/AutowiredCompletionAwareInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Draw\Bundle\TesterBundle\PHPUnit\Extension\SetUpAutowire;
-
-interface AutowiredCompletionAwareInterface extends AutowiredInterface
-{
-    public function postAutowire(): void;
-}
